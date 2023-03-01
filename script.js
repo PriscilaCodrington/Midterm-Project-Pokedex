2dbd9ba10ec536fd7881530d646e32f5c50c377b
PriscilaCodrington <priscilacodrington@gmail.com>
2023-03-01 18:08:00 +0100

    Project finish: New function to print the types, all css

--- script.js
+++ script.js
@@ -4,97 +4,97 @@
 
 const pokemonsarray = [
   {
-    thumbnail: 'media/001.png',
+    thumbnail: 'assets/001.png',
     id:'N°01' ,
     name: 'bulbasaur',
     type: ['plant', 'poison'],
   },
   {
-    thumbnail: 'media/002.png',
+    thumbnail: 'assets/002.png',
     id:'N°02',
     name: 'charmander',
     type: ['fire'],
   },
   {
-    thumbnail: 'media/003.png',
+    thumbnail: 'assets/003.png',
     id:'N°03',
     name: 'caterpie',
     type: ['bug'],
   },
   {
-    thumbnail: 'media/004.png',
+    thumbnail: 'assets/004.png',
     id:'N°04',
     name: 'kaluna',
     type: ['bug', 'poison'],
   },
   {
-    thumbnail: 'media/005.png',
+    thumbnail: 'assets/005.png',
     id:'N°05',
     name: 'ratata',
     type: ['plant'],
   },
   {
-    thumbnail: 'media/006.png',
+    thumbnail: 'assets/006.png',
     id:'N°06',
     name: 'pikachu',
     type: ['normal'],
   },
   {
-    thumbnail: 'media/007.png',
+    thumbnail: 'assets/007.png',
     id:'N°07',
     name: 'vulpix',
     type: ['fire', 'plant'],
   },
   {
-    thumbnail: 'media/008.png',
+    thumbnail: 'assets/008.png',
     id:'N°08',
     name: 'jigglypuff',
     type: ['normal', 'fairy'],
   },
   {
-    thumbnail: 'media/009.png',
+    thumbnail: 'assets/009.png',
     id:'N°09',
     name: 'ice',
     type: ['bug'],
   },
   {
-    thumbnail: 'media/010.png',
+    thumbnail: 'assets/010.png',
     id:'N°10',
     name: 'ground',
     type: ['electrical'],
   },
   {
-    thumbnail: 'media/011.png',
+    thumbnail: 'assets/011.png',
     id:'N°11',
     name: 'ghost',
     type: ['poison'],
   },
   {
-    thumbnail: 'media/012.png',
+    thumbnail: 'assets/012.png',
     id:'N°12',
     name: 'deer',
     type: ['fire'],
   },
   {
-    thumbnail: 'media/013.png',
+    thumbnail: 'assets/013.png',
     id:'N°13',
     name: 'dark',
     type: ['fairy'],
   },
   {
-    thumbnail: 'media/014.png',
+    thumbnail: 'assets/014.png',
     id:'N°14',
     name: 'stell',
     type: ['normal', 'electrical'],
   },
   {
-    thumbnail: 'media/015.png',
+    thumbnail: 'assets/015.png',
     id:'N°15',
     name: 'cat',
     type: ['fire'],
   },
   {
-    thumbnail: 'media/016.png',
+    thumbnail: 'assets/016.png',
     id:'N°16',
     name: 'rock',
     type: ['fairy', 'poison'],
@@ -102,34 +102,69 @@ const pokemonsarray = [
 ]
 
 
-
-pokemonsarray.forEach(pokemon => {singlePokemonCard(pokemon)})
-
+//Function to iterated the array
+pokemonsarray.forEach(pokemon => {singlePokemonCard(pokemon)
+    })
+//Function to print a Card
 function singlePokemonCard (pokemon) {
 
     let card = document.createElement("div");
     document.querySelector(".all-pokemons-cards").appendChild(card);
-    card.className = "pokeCard";
+    card.className = "poke-card";
 
     let pokeImg = document.createElement("img");
     pokeImg.src = pokemon.thumbnail;
-    pokeImg.className = "pokeImg";
-    document.querySelector(".pokeCard").appendChild(pokeImg);
+    pokeImg.className = "poke-img";
+    card.appendChild(pokeImg);
+
 
     let pokeId = document.createElement("p");
     pokeId.innerHTML= pokemon.id;
-    document.querySelector(".pokeCard").appendChild(pokeId);
+    card.appendChild(pokeId);
 
     let pokeName = document.createElement("h2");
     pokeName.innerHTML= pokemon.name;
-    document.querySelector(".pokeCard").appendChild(pokeName);
+    card.appendChild(pokeName);
+  
+    printTypes(pokemon.type, card);
 
-
+    console.log(pokemon.type);
 
 }
 
+// Print Type and Adding a new class for every span Function
 
-/*thumbnail: 'media/001.png',
-    id:'N°01' ,
-    name: 'bulbasaur',
-    type: ['plant', 'poison'], */
+function printTypes(typesElement, card){
+
+    for(let i=0; i<typesElement.length; i++){
+        let pokeType = document.createElement("span");
+        pokeType.innerHTML = typesElement[i];
+        pokeType.className = "poke-type";
+        card.appendChild(pokeType);
+    
+    switch(typesElement[i]){
+      case 'fairy':
+        pokeType.classList.add('fairy');
+      break;
+      case 'poison':
+        pokeType.classList.add('poison');
+      break;
+      case 'fire':
+        pokeType.classList.add('fire');
+      break;
+      case 'normal':
+        pokeType.classList.add('normal');
+      break;
+      case 'electrical':
+        pokeType.classList.add('electrical'); 
+      break;
+      case 'plant':
+        pokeType.classList.add('plant'); 
+      break;
+      case 'bug':
+        pokeType.classList.add('bug'); 
+      break;
+    }
+}
+}
+
